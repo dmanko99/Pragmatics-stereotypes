1939968f45a1319b69a48e562ec952ec220e6020
Dean Manko <dmanko@stanford.edu>
2021-01-18 18:46:58 -0500

    updated analysis file

--- data & analysis/TES_norming_1/rscripts/analysis.R
+++ data & analysis/TES_norming_1/rscripts/analysis.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(jsonlite)
 library(bootstrap)
 library(brms)
+library(ggwordcloud)
 
 source("helpers.R")
 
@@ -12,13 +13,13 @@ options(mc.cores = parallel::detectCores())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))    
 
 # Load the data
-df = read.csv("../data/Pilot/tesnorming-trials.csv", header = TRUE)%>%
+df = read.csv("../data/Pilot/Live/tesnorming_pilot-trials.csv", header = TRUE)%>%
   filter(slide_type != "bot_check")
 
-subinfo = read.csv("../data/Pilot/tesnorming-subject_information.csv", header = TRUE)
+subinfo = read.csv("../data/Pilot/Live/tesnorming_pilot-subject_information.csv", header = TRUE)
 
 # DETERMINE MEAN COMPLETION TIME
-mean(df$Answer.time_in_minutes)
+mean((read.csv("../data/Pilot/Live/tesnorming_pilot-time_in_minutes.csv", header = TRUE))$time_in_minutes)
 
 # HELPER SCRIPTS
 dodge = position_dodge(.9)
@@ -33,7 +34,7 @@ length(unique(df$workerid))
 
 # MAKE EXCLUSIONS
 wrong_responses <- df %>%
-  filter((tag == "exclusion_wrong" & response > 50) | (tag == "exclusion_right" && response < 50))
+  filter(tag == "exclusion" & response == "fail")
 blacklist <- unique(wrong_responses$workerid)
 
 df <- df %>%
@@ -67,113 +68,57 @@ levels(df$nameCategory) <- c("white", "black")
 # RENAME COLUMNS
 df <- df %>%
   rename(stereotype = tag,
-         item = story,
          name = first)
 
-# VISUALIZE RESPONSE BY STEREOTYPE AND NAME
-df %>% 
-  filter(type == "critical") %>%
-  filter(stereotype == "evoke") %>%
-  ggplot(aes(x = response, fill = nameCategory)) +
-  geom_histogram(alpha = 0.5, position = "identity") +
-  facet_wrap(~item) 
+# DROP IRRELEVANT COLUMNS
+df <- subset(df, select = -c(audio, image, slide_number, slide_type, error, proliferate.condition))
 
-byStereotypeNameCategory <- df %>%
-  filter(type == "critical") %>%
-  group_by(nameCategory, stereotype) %>%
-  summarize(Mean = mean(response), 
-            CILow =ci.low(response),
-            CIHigh =ci.high(response)) %>%
-  ungroup() %>%
-  mutate(YMin = Mean - CILow,
-         YMax = Mean + CIHigh)
+# SET UP BASIC DATAFRAMES
+critical_df <- filter(df, type == "critical")
 
-ggplot(byStereotypeNameCategory, aes(x=nameCategory, y=Mean, fill = stereotype)) +
-  # facet_wrap(~kind, scales = "free") +
-  theme_bw() +
-  scale_fill_grey() +
-  geom_bar(stat="identity",position = "dodge") +
-  theme(axis.text.x=element_text(angle=20,hjust=1,vjust=1)) +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),size = 0.25,width= 0.025,position = dodge) +  
-  labs(x = "Race", y = "Mean rating", fill = "Stereotype") 
+filler_df <- filter(df, type == "filler")
 
+# SET UP DATAFRAMES BY STORY
+storylist <- unique(critical_df$story)
 
-# BY STEREOTYPE AND NAME CATEGEORY, BY WORKER ID
-byStereotypeNameCategoryWorker <- df %>%
-  filter(type == "critical") %>%
-  group_by(nameCategory, stereotype, workerid) %>%
-  summarize(Mean = mean(response), 
-            CILow =ci.low(response),
-            CIHigh =ci.high(response)) %>%
-  ungroup() %>%
-  mutate(YMin = Mean - CILow,
-         YMax = Mean + CIHigh)
-ggplot(byStereotypeNameCategoryWorker, aes(x=nameCategory, y=Mean, fill = stereotype)) +
-  # facet_wrap(~kind, scales = "free") +
-  facet_wrap(~workerid) +
-  theme_bw() +
-  scale_fill_grey() +
-  geom_bar(stat="identity",position = "dodge") +
-  theme(axis.text.x=element_text(angle=20,hjust=1,vjust=1)) +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),size = 0.25,width= 0.025,position = dodge) +  
-  labs(x = "Race", y = "Mean rating", fill = "Stereotype") 
+byStoryResponse <- critical_df %>%
+  select(name, response, story, nameCategory) %>%
+  group_split(story, nameCategory)
 
+# FREQUENCY OF RESPONSE BY STORY - to be expanded on with more data
+df_freq <- critical_df %>%
+  group_by(story) %>%
+  count('response')
+  
 
-# VISUALIZE RESPONSE BY NAME AND ITEM
-byNameCategoryItem <- df %>%
-  filter(type == "critical") %>%
-  group_by(nameCategory, item, stereotype) %>%
-  summarize(Mean = mean(response), 
-            CILow =ci.low(response),
-            CIHigh =ci.high(response)) %>%
-  ungroup() %>%
-  mutate(YMin = Mean - CILow,
-         YMax = Mean + CIHigh)
+# OBTAINING LIST OF RESPONSES BY STORY
+get_responses <- function(prompt_name) {
+  response_list_white <- list()
+  response_list_black <- list()
+  subframe_white <- filter(df, story == prompt_name, nameCategory == "white") 
+  subframe_black <- filter(df, story == prompt_name, nameCategory == "black")
+  for (i in subframe_white$response) {
+    response_list_white <- c(response_list_white, i)
+  }
+  for (j in subframe_black$response) {
+    response_list_black <- c(response_list_black, j)
+  }
+  return(cbind(response_list_white, response_list_black))
+}
+  
+# dataframe of all response lists by story — to be updated when more data is available
+# responses_white <- c()
+# responses_black <- c()
+# for (a in storylist) {
+#   subf <- get_responses(a)
+#   responses_white <- c(responses_white, subf[1:(length(subf)/2)])
+#   responses_black <- c(responses_black, subf[(length(subf)/2):(length(subf))])
+# }
+# byStoryLists <- data.frame(x = storylist, y = responses_by_story)
+# responses_by_story <- data.frame(storylist, )
 
-byNameCategoryItem %>%
-  # CAN BE 'evoke' or 'repress'
-  filter(stereotype == "evoke") %>%
-  ggplot(aes(x=item, y=Mean, fill = nameCategory)) +
-  theme_bw() +
-  scale_fill_grey() +
-  geom_bar(stat="identity",position = "dodge") +
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=1)) +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),size = 0.25,width= 0.025,position = dodge) +  
-  labs(x = "Item", y = "Mean rating", fill = "Name") 
-
-# MEAN CENTERING AND BRMS ANALYSIS
-df$response_ctr <- scale(df$response, center = TRUE, scale = FALSE)
-df$response_ctr <- df$response_ctr - mean(df$response_ctr)
-df$stereotype = relevel(df$stereotype, ref = "repress")
-
-##center affiliation measurements?
-dfAffiliation$response_ctr <- scale(df$response, center = TRUE, scale = FALSE)
-dfAffiliation$response_ctr <- df$response_ctr - mean(df$response_ctr)
-dfAffiliation$stereotype = relevel(df$stereotype, ref = "repress")
-
-
-m <- brm(
-  response_ctr ~ nameCategory * stereotype + (1 + nameCategory|item) + (1 + stereotype|name) + (1 + nameCategory*stereotype|workerid), 
-  data = df %>% filter(type == "critical"),
-  control = list(adapt_delta = 0.99, max_treedepth = 15),
-  family = gaussian(),
-  seed = 123,
-  iter = 5000
-)
-
-summary(m)
-
-m_affiliation <- brm(
-  response_ctr ~ nameCategory * stereotype * affiliation + (1 + nameCategory*stereotype|workerid) +
-    (1 + stereotype*affiliation|nameCategory),
-  data = dfAffiliation %>% filter(type == "critical", affiliation > -1),
-  control = list(adapt_delta = 0.9, max_treedepth = 15),
-  family = gaussian(),
-  seed = 123,
-  iter = 5000
-)
-
-summary(m_affiliation)
-# COEFFICIENT IS GREATER THAN 0: 
-
-hypothesis(m, "nameCategoryTRUE:stereotypeevoke > 0")
+# VISUALIZE RESPONSES BY STORY
+wordcloud_by_story <- ggplot(critical_df, aes(label = response, color = nameCategory)) +
+  geom_text_wordcloud() +
+  theme_minimal() +
+  facet_wrap(~story)
