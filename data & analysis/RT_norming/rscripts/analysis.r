63fa48d5a7aa2e49d2fc0b6415e555514bc3af48
Dean Manko <dmanko@stanford.edu>
2021-03-16 18:08:34 -0700

    analysis and other fixes

--- data & analysis/RT_norming/rscripts/analysis.r
+++ data & analysis/RT_norming/rscripts/analysis.r
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(lme4)
 library(lmerTest)
+library(ggplot2)
+library(bootstrap)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
@@ -14,7 +16,7 @@ ci.high <- function(x,na.rm=T) {
 
 # Import dataframe
 #df <- read_csv("../data/RT_mainNorming/tesRT-trials.csv", header = TRUE) %>%
-df = read.csv("../data/Pilot/tesRT_pilot2-trials.csv", header = TRUE) %>%
+df = read.csv("../data/Pilot/tesRT_pilot2-trials2.csv", header = TRUE) %>%
   # remove bot check slides
   filter(!(type == "bot_check")) %>%
   # remove the practice trial
@@ -44,6 +46,22 @@ excludeList <- (by_trial %>%
 df <- df %>%
   filter(!(workerid %in% excludeList))
 
+# Set up different regions
+df <- df %>%
+  mutate(region_old = region) %>%
+  mutate(region = case_when(lead(region_old) == "crit" ~ "precritical_1",
+                            lag(region_old) == "crit" ~ "spillover_1",
+                            region_old == "crit" ~ "crit",
+                            TRUE ~ "other")) %>%
+  mutate(region_old = region) %>%
+  mutate(region = case_when(region_old == "precritical_1" ~ "precritical_1",
+                            region_old == "spillover_1" ~ "spillover_1",
+                            region_old == "crit" ~ "crit",
+                            lead(region_old) == "precritical_1" ~ "precritical_2",
+                            lag(region_old) == "spillover_1" ~ "spillover_2",
+                            TRUE ~ "other")) %>%
+  select(-region_old)
+
 # Identify per-trial rt (and exclude data from trials > or < 2.5 sds from mean)
 
 df <- df %>%
@@ -66,30 +84,34 @@ df$logtrial = log(df$trial_no + 1)
 
 # Regress word length and overall trial number onto log RT (ie, subtract out the variance contributed by these factors.)
 
-resid_model = lmer(logrt ~ wordlen * logtrial + (wordlen | workerid), df, REML=F)  # estimates the random effects of per participant reading time differences
-
+resid_model = lmer(logrt ~ wordlen + (0+wordlen|workerid), data = df)
 res = resid(resid_model)
 
 df$residuals <- res
 
 # Predict residualized reading time from race, consistency, and interaction
 
-criticaldf <- filter(df, type == "critical")
+## For items in the "critical" region
+criticaldf <- filter(df, region == "crit", type == "critical")
 criticaldf$bias <- factor(criticaldf$bias)
 criticaldf$nameCategory <- factor(criticaldf$nameCategory)
 criticaldf <- criticaldf %>%
   mutate(consistency = case_when(bias == nameCategory ~ "consistent", TRUE ~ "inconsistent"))
 
+criticaldf$ctr_nameCategory = as.numeric(criticaldf$nameCategory) - mean(as.numeric(criticaldf$nameCategory))
+criticaldf$ctr_consistency = as.numeric(factor(criticaldf$consistency)) - mean(as.numeric(factor(criticaldf$consistency)))
 
-model = lmer(residuals ~ nameCategory * consistency + 
-               ((nameCategory * consistency) | trial_id) + ((nameCategory * consistency) | workerid), criticaldf,
-             REML = F)
+
+model = lmer(residuals ~ logtrial * ctr_nameCategory * ctr_consistency + 
+               (1 + (logtrial * ctr_nameCategory * ctr_consistency) | trial_id) + 
+               (1 + (logtrial * ctr_nameCategory * ctr_consistency) | workerid), 
+             criticaldf, REML = F)
 
 summary(model)
 
 # Visualize raw & transformed data
 rawgrouped = criticaldf %>% 
-  group_by(type, region, trial_no) %>% 
+  group_by(type, region) %>% 
   summarize(rt = mean(rt),CILow=ci.low(rt),CIHigh=ci.high(rt)) %>% 
   ungroup() %>% 
   mutate(YMin=rt-CILow,YMax=rt+CIHigh)
@@ -97,14 +119,13 @@ rawgrouped = criticaldf %>%
 ggplot(rawgrouped, aes(x=region, y=rt, color=type, group=type)) + 
   geom_point()  + 
   geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25) +
-  geom_line() + 
-  colscale +
+  geom_line() +
   labs(x='Sentence region', y='Mean raw RT (ms)', fill='Type')
 
 ggplot(rawgrouped, aes(x=trial_no, y=rt, col=type)) + 
   geom_point(alpha=.2)  + 
   geom_smooth() + 
-  colscale +
+  #colscale +
   labs(x='Item order (#RCs seen)', y='Raw RTs (ms)', fill='Type')
 
 grouped = criticaldf %>% 
@@ -113,6 +134,6 @@ grouped = criticaldf %>%
 ggplot(grouped, aes(x=trial_no, y=rt, col=type)) + 
   geom_point() +
   geom_smooth(method="lm") + 
-  colscale +
-  labs(x='Item order (#RCs seen)', y='Length- and order-corrected log RTs (ms)', fill='Type')
+  #colscale +
+  labs(x='Item order (#sentences seen)', y='Length- and order-corrected log RTs (ms)', fill='Type')
 
